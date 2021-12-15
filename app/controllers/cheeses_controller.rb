3311e28b8539627c0bc8edcab7f19e2175868f1d
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-12-15 13:55:53 -0800

    commit tree fa866b114b3ca65889831f2463c1f5b6d5ab3e95

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -8,8 +8,12 @@ class CheesesController < ApplicationController
 
   # GET /cheeses/:id
   def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    cheeses = Cheese.find_by(id: params[:id])
+    if cheeses
+      render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: {error: "Could not find that cheese!"}, status: :not_found
+    end
   end
 
 end
